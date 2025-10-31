1036c41b785cc84ba45dc5d62cbb9718853dd3b5
PKQandBaaad <pkqypkj@outlook.com>
2025-10-31 13:27:59 +0800

    More Update
    sourcecode: add gpl3 license​
    add d for windows​

--- randfill.c
+++ randfill.c
@@ -1,3 +1,22 @@
+/*
+ * This file is part of RandFill.
+ *
+ * RandFill is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * RandFill is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with RandFill.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Copyright (C) 2025 OpenHelloMiddle Developers
+ */
+
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
